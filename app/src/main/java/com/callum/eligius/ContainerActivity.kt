bfbd43f5f53a7c338ac854c39a92389930c3a9e1
Callum Fiekert <fiekertc@gmail.com>
2022-01-23 05:19:10 +0000

    Added initial Activity, Fragments, and Nav Component

--- app/src/main/java/com/callum/eligius/ContainerActivity.kt
+++ app/src/main/java/com/callum/eligius/ContainerActivity.kt
@@ -2,6 +2,9 @@ package com.callum.eligius
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 import timber.log.Timber.i
 
@@ -10,6 +13,11 @@ class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainer)
+
+        bottomNav.setupWithNavController(navController)
 
         Timber.plant(Timber.DebugTree())
 

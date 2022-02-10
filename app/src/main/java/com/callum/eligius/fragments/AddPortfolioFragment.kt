9b5a758b182d97bab44190c1b6fe63b098f73c4f
Callum Fiekert <fiekertc@gmail.com>
2022-02-10 16:44:53 +0000

    Can now add portfolios

--- app/src/main/java/com/callum/eligius/fragments/AddPortfolioFragment.kt
+++ app/src/main/java/com/callum/eligius/fragments/AddPortfolioFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.NumberPicker
+import androidx.core.view.size
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.callum.eligius.R
 import com.callum.eligius.adapters.AddCoinAdapter
 import com.callum.eligius.databinding.FragmentAddPortfolioBinding
 import com.callum.eligius.main.Main
+import com.callum.eligius.models.CoinModel
+import com.callum.eligius.models.PortfolioModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AddPortfolioFragment : Fragment() {
 
@@ -21,6 +28,7 @@ class AddPortfolioFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = activity?.application as Main
     }
 
     override fun onCreateView(
@@ -43,14 +51,38 @@ class AddPortfolioFragment : Fragment() {
             loadAdverts()
         }
 
-//        fragBinding.cancel.setOnClickListener {
-//            val fragment = PortfolioListFragment()
-//            val transaction = manager.beginTransaction()
-//            transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-//            transaction.replace(R.id.fragmentContainer, fragment)
-//            transaction.addToBackStack(null)
-//            transaction.commit()
-//        }
+        fragBinding.cancel.setOnClickListener {
+            val fragment = PortfolioListFragment()
+            val transaction = manager.beginTransaction()
+            transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            transaction.replace(R.id.fragmentContainer, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        fragBinding.addPortfolio.setOnClickListener {
+            val newID = UUID.randomUUID().toString()
+            var name = fragBinding.portfolioName.text.toString()
+            var value = 0
+            var coins : MutableList<CoinModel> = emptyList<CoinModel>().toMutableList()
+            for (num in 0 until fragBinding.recyclerView2.size) {
+                var card = fragBinding.recyclerView2.findViewHolderForAdapterPosition(num)
+                var coinAmount = card?.itemView?.findViewById<EditText>(R.id.coinAmount)?.text.toString()
+                var coinSelected = card?.itemView?.findViewById<NumberPicker>(R.id.coinSelected)?.value.toString()
+
+                coins.add(CoinModel(5, coinSelected, coinAmount, 0))
+            }
+
+            app.portfoliosStore.create(PortfolioModel(0, name, value, coins))
+            println(app.portfoliosStore)
+
+            val fragment = PortfolioListFragment()
+            val transaction = manager.beginTransaction()
+            transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            transaction.replace(R.id.fragmentContainer, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
 
         loadAdverts()
 
--- app/src/main/java/com/callum/eligius/models/CoinModel.kt
+++ app/src/main/java/com/callum/eligius/models/CoinModel.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class CoinModel(var id: Long = 0, val name: String = "N/A", val amount: Int = 0, val value: Int = 0) : Parcelable
+data class CoinModel(var id: Long = 0, val name: String = "N/A", val amount: String = "0", val value: Int = 0) : Parcelable
